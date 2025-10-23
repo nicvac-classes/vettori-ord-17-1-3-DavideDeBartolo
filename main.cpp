cdc05407e224b0ab1d25e378adebdc5e92e752e2
DavideDeBartolo <davide.debartolo@marconibari.edu.it>
2025-10-23 14:20:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
-using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+
+void bubbleSort(std::vector<int>& v) {
+    int n = v.size();
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (v[j] > v[j + 1]) {
+                std::swap(v[j], v[j + 1]);
+            }
+        }
+    }
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+void visualizzaVettore(const std::vector<int>& v) {
+    for (int num : v) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+}
+
+std::vector<int> merge(const std::vector<int>& v1, const std::vector<int>& v2) {
+    std::vector<int> merged(v1.size() + v2.size());
+    std::merge(v1.begin(), v1.end(), v2.begin(), v2.end(), merged.begin());
+    return merged;
+}
+
+int ricercaBinaria(const std::vector<int>& v, int valore) {
+    int left = 0, right = v.size() - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (v[mid] == valore
